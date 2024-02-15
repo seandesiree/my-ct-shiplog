31e518f6b0d72ff510c8ac9749b76f1bd067c021
seandesiree <sean.desiree@gmail.comgit config --global user.email sean.desiree@gmail.comgit versioncdgit config --global user.email sean.desiree@gmail.com>
2024-02-15 14:48:35 -0500

    updated words

--- hello.py
+++ hello.py
@@ -1,4 +1,13 @@
 print("hello")
 print("I love you")
+if 7 > 5:
+    Print("Seven is greater than five")
+favorite_quote = '''
+It's always best to exhaust all 
+options before asking for 
+help'''
 
+print(favorite_quote)
 
+sum = 5 + 3
+print("The sum of 5 and 3 is:", sum)
