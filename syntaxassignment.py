e749eccd3d9a49015be916e16e5b625cc7acf51b
seandesiree <sean.desiree@gmail.comgit config --global user.email sean.desiree@gmail.comgit versioncdgit config --global user.email sean.desiree@gmail.com>
2024-02-17 15:57:19 -0500

    homework

---
+++ syntaxassignment.py
@@ -0,0 +1,95 @@
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+question = "How are you feeling today?"
+print(question)
+
+mood = "happy"
+if mood == "happy":
+    print("That's great to hear!")
+else:
+    print("I Hope your day gets better!")
+
+mood = "excited"
+
+if mood == "excited":
+    print("Yay! Let's have fun.")
+else:
+    print("Let's find something fun to do!")
+
+# Python, oh Python, so clear and so neat
+# With every new challenge, you're so hard to beat.
+# Python, oh Python, so clear and so neat 
+#I know there will be many hours I will sit in my seat
+#Python, oh Python, so clear and so neat
+#With syntax errors you will not let me cheat"
+
+poem = '''Python, in the realm of code you shine,
+With simplicity and grace, you're truly divine.
+Python, in the realm of code you shine,
+I know that with you my coding will be fine.
+Python, in the realm of code you shine,
+Learning to use you is worth every dime.
+Python, in the realm of code you shine,
+I take my time with you line after line'''
+
+print(poem)
+
+# This line is from my first poem
+#With syntax errors you will not let me cheat"
+
+# This line is from my first poem
+'''I take my time with you line after line'''
+
+pi_value = 3.14
+userAge = 25
+user_location = "New York"
+MAXLIMIT = 1000
+
+variable_a = "Hello, World!"
+print(type(variable_a))
+variable_b = 23
+print(type(variable_b))
+variable_c = 3.14
+print(type(variable_c))
+variable_d = True
+print(type(variable_d))
+
+dynamic_variable1 = "This is a string"
+print(type(dynamic_variable1))
+
+dynamic_variable2 = 100
+print(type(dynamic_variable2))
+
+dynamic_variable3 = 25.5
+print(type(dynamic_variable3))
+
+a = 19
+b = 25
+a, b = b, a
+print(a) 
+print(b)
+
+x = 9
+square_root = x ** 0.5
+print(square_root)
+
+c = True
+d = False 
+
+print(c and d)
+print(c or d)
+print(not d)
+
+e = 2+4*8-16
+print(e)
+
+f = 2+4*(8-16) 
+print(f)
+
+g = 4/2 and 5<1
+print(g)
